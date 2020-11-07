566b809a1f2723d5d794369c60e0bade4bbc4ddc
Daria Frolova <darya.frolova@rt.ru>
2020-11-07 14:30:39 +0300

    Добавила дз про лэндинг и пагинацию

--- landing/app/views.py
+++ landing/app/views.py
@@ -1,18 +1,25 @@
 from collections import Counter
 
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 
 # Для отладки механизма ab-тестирования используйте эти счетчики
 # в качестве хранилища количества показов и количества переходов.
 # но помните, что в реальных проектах так не стоит делать
 # так как при перезапуске приложения они обнулятся
+
 counter_show = Counter()
 counter_click = Counter()
 
 
 def index(request):
     # Реализуйте логику подсчета количества переходов с лендига по GET параметру from-landing
-    return render_to_response('index.html')
+    from_landing = request.GET.get('from-landing')
+    if from_landing == 'test':
+        counter_click['test'] += 1
+    elif from_landing == 'original':
+        counter_click['original'] += 1
+    # print(counter_click['original'])
+    return render(request, 'index.html')
 
 
 def landing(request):
@@ -20,13 +27,33 @@ def landing(request):
     # в зависимости от GET параметра ab-test-arg
     # который может принимать значения original и test
     # Так же реализуйте логику подсчета количества показов
-    return render_to_response('landing.html')
+    ab_type = request.GET.get('ab_test_arg', 'original')
+    if ab_type == 'test':
+        template = 'landing_alternate.html'
+        counter_show['test'] += 1
+    else:
+        template = 'landing.html'
+        counter_show['original'] += 1
+    # print(counter_show['original'])
+    return render(request, template)
 
 
 def stats(request):
     # Реализуйте логику подсчета отношения количества переходов к количеству показов страницы
     # Для вывода результат передайте в следующем формате:
-    return render_to_response('stats.html', context={
-        'test_conversion': 0.5,
-        'original_conversion': 0.4,
+    # print(counter_show['original'])
+    # print(counter_click['original'])
+    if counter_show['test'] > 0:
+        test_conversion = round(int(counter_click['test'])/int(counter_show['test']), 1)
+    else:
+        test_conversion = 0
+    # print(test_conversion)
+    if counter_show['original'] > 0:
+        original_conversion = round(int(counter_click['original'])/int(counter_show['original']), 1)
+    else:
+        original_conversion = 0
+    # print(original_conversion)
+    return render(request, 'stats.html', context={
+        'test_conversion': test_conversion,
+        'original_conversion': original_conversion,
     })
--- pagination/app/views.py
+++ pagination/app/views.py
@@ -1,5 +1,11 @@
+from urllib.parse import urlencode
+
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
+import csv
+
+from pagination.app import settings
 
 
 def index(request):
@@ -7,13 +13,29 @@ def index(request):
 
 
 def bus_stations(request):
-    current_page = 1
-    next_page_url = 'write your url'
+    items_by = 10
+    list_of_stations = []
+    with open(settings.BUS_STATION_CSV, encoding='cp1251',  newline='') as csv_file:
+        reader = csv.DictReader(csv_file)
+        for row in reader:
+            station_data = {'Name': row['Name'], 'Street': row['Street'], 'District': row['District']}
+            list_of_stations.append(station_data)
+        print(list_of_stations)
+        paginator = Paginator(list_of_stations, items_by)
+        page_number = request.GET.get('page', 1)
+        current_page_obj = paginator.get_page(page_number)
+
+        next_page, previous_page = None, None
+        if current_page_obj.has_next():
+            next_page = reverse('bus_stations') + '?' + urlencode({'page': current_page_obj.next_page_number()})
+        if current_page_obj.has_previous():
+            previous_page = reverse('bus_stations') + '?' + urlencode({'page': current_page_obj.previous_page_number()})
+
+
     return render(request, 'index.html', context={
-        'bus_stations': [{'Name': 'название', 'Street': 'улица', 'District': 'район'},
-                         {'Name': 'другое название', 'Street': 'другая улица', 'District': 'другой район'}],
-        'current_page': current_page,
-        'prev_page_url': None,
-        'next_page_url': next_page_url,
+        'bus_stations': current_page_obj,
+        'current_page': page_number,
+        'prev_page_url': previous_page,
+        'next_page_url': next_page
     })
 
